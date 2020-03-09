e1a0ad512919488d0abfee1d9cbbceda55ec01bc
onrush <moniruzzaman.eng1995@gmail.com>
2020-03-09 18:51:39 +0200

    feat:award has been made based on score

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
 
 //1961,Fortran,intel,1995,Javascript
 // Declare all the answer and question for the program
-let score;
 const ques1 = prompt("Which Year first web page was invented: ");
 const ques2 = prompt("What was the first commercially avaiable language: ");
 const ques3 = prompt("Which company first introduces the microprocessor: ");
@@ -20,6 +19,8 @@ const input3 = ques3.toUpperCase();
 const input4 = ques4.toUpperCase();
 const input5 = ques5.toUpperCase();
 
+let score = 0;
+
 const answer1= 1961;
 const answer2= "FORTRAN";
 const answer3= "INTEL";
@@ -30,29 +31,51 @@ if(answer1 !== input1){
     document.writeln(`1.You have typed the wrong answer,the correct answer is ${answer1}.`);
 }else {
     document.writeln(`1.You have typed the correct answer.`);
+    score++;
 }
 
-if(answer2 !== input2){
-    document.writeln(`2.You have typed the wrong answer,the correct answer is ${answer2}.`);
-}else {
+if(answer2 == input2){
     document.writeln(`2.You have typed the correct answer.`);
+    score++;
+}else {
+    //document.writeln(`2.You have typed the correct answer.`);
+    document.writeln(`2.You have typed the wrong answer,the correct answer is ${answer2}.`);
+    //score++;
 }
 
 if(answer3 !== input3){
     document.writeln(`3.You have typed the wrong answer,the correct answer is ${answer3}.`);
 }else {
     document.writeln(`3.You have typed the correct answer.`);
+    score++;
 }
 
 if(answer4 !== input4){
     document.writeln(`4.You have typed the wrong answer,the correct answer is ${answer4}.`);
 }else {
     document.writeln(`4.You have typed the correct answer.`);
+    score++;
 }
 if(answer5 !== input5){
     document.writeln(`5.You have typed the wrong answer,the correct answer is ${answer5}.`);
 }else {
     document.writeln(`5.You have typed the correct answer.`);
+    score++;
 }
+
+for (var i= 0;i<score;i++){
+    if(score == 5){
+        document.writeln(`<bold>You have earned <italic>Gold</italic>medal</bold>`);
+    }else if (score == 4){
+        document.writeln(`<bold>You have earned <italic>Silver</italic>medal</bold>`);
+    }else if (score == 3){
+        document.writeln(`<bold>You have earned <italic>Bronze</italic>medal</bold>`);
+    }else if (score ==2){
+        document.writeln(`<bold>Sorry,you did not achive any medal this time</bold>`);
+    }else if (score<=1){
+        document.writeln(`<bold>Sorry,you did not achive any medal this time,come next time.</bold>`);
+    }
+        
+    }
 
 
